ac2a9fb58a7c7e419cbff3f3728cb0c0de13cfe3
Saurabh Jain <jainsaurabh86@gmail.com>
2018-05-28 12:04:55 -0400

    Enabled ag-grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { AgGridModule } from 'ag-grid-angular';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [ AppComponent ],
+  imports: [BrowserModule, AgGridModule.withComponents([])],
   providers: [],
   bootstrap: [AppComponent]
 })
